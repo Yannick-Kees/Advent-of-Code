e22cd790ba3dfc1e0e41e729a18ed168234908f0
Yannick <yannick.kees@dlr.de>
2025-12-01 13:29:39 +0100

    First commit

---
+++ src/main.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2025 Yannick Kees
+#
+# SPDX-License-Identifier: MIT
+"""Description."""
+import logging
+
+import numpy as np
+
+if __name__ == "__main__":
+    logger = logging.getLogger(__name__)
+    logger.info(np.zeros((1, 1)))
