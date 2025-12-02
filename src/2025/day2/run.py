fd0eb99f0d1fa3453835ec772aa73ddeb28637be
Yannick <yannick.kees@dlr.de>
2025-12-02 13:24:14 +0100

    20251202_1324

--- src/2025/day2/run.py
+++ src/2025/day2/run.py
@@ -5,18 +5,40 @@
 
 from src.base_class import riddle
 
-TOTAL_SIZE = 100
-START_POINT = 50
+
+class code:
+    def __init__(self, section):
+        self.start, self.stop = map(int, section.split("-"))
+
+
+def is_valid(x: str) -> bool:
+    return x[: len(x) // 2] == x[len(x) // 2 :]
+
+
+def is_really_valid(x: str) -> bool:
+    return any(x == (x[:i] * (len(x) // i))[: len(x)] for i in range(1, len(x) // 2 + 1))
 
 
 class day1(riddle):
     def __init__(self, day, mode):
         super().__init__(day, mode)
 
-    def a(self):
-        pass
+    def a(self, function=is_valid):
+        all_codes = [code(line) for line in self.lines[0].split(",")]
+        print(
+            sum(
+                [
+                    sum([x for x in range(c.start, c.stop + 1) if function(str(x))])
+                    for c in all_codes
+                ],
+            ),
+        )
+
+    def b(self):
+        self.a(function=is_really_valid)
 
 
 if __name__ == "__main__":
-    x = day1(day=2, mode="test")
+    x = day1(day=2, mode="full")
     x.a()
+    x.b()
